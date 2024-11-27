210f83f59de3f65b5059bb84ab595ff506877748
carlcarl1231 <carljaphethgomez@sksu.edu.ph>
2024-11-27 17:55:47 +0800

    Add index.php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IGIT</title>
+</head>
+<body>
+
+<h1>YOKOUSOU GOMI DOMO</h1>
+   <h2>OREWA DAI MAOU DA</h2> 
+</body>
+</html>
